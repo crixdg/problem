8c24671390bfa4c09fc16035e8c29b7f6ff74d7e
Crix Dang <crix.dang@gmail.com>
2026-02-02 16:00:35 +0700

    up Mon Feb  2 09:00:35 AM UTC 2026

--- sources/a.cpp
+++ sources/a.cpp
@@ -2,21 +2,58 @@
 using namespace std;
 
 int main() {
-    int q;
-    cin >> q;
-    unordered_map<long long, long long> mp;
+    int n, q;
+    cin >> n >> q;
+    set<int> st;
+    string T;
+    cin >> T;
+    for (int i = 0; i < T.size(); i++) {
+        if (T[i] == '1') { st.insert(i); }
+    }
     while (q--) {
         int op;
-        long long k, v;
-
         cin >> op;
-        if (op == 0) {
-            cin >> k >> v;
-            mp[k] = v;
-        } else if (op == 1) {
-            cin >> k;
-            cout << mp[k] << '\n';
+        int k;
+        cin >> k;
+        switch (op) {
+        case 0:
+            st.insert(k);
+            break;
+        case 1: {
+            auto it = st.find(k);
+            if (it != st.end()) { st.erase(it); }
+            break;
+        }
+        case 2: {
+            auto it = st.find(k);
+            if (it != st.end()) {
+                cout << 1 << '\n';
+            } else {
+                cout << 0 << '\n';
+            }
+            break;
+        }
+        case 3: {
+            auto it = st.upper_bound(k - 1);
+            if (it == st.end()) {
+                cout << -1 << '\n';
+            } else {
+                cout << *it << '\n';
+            }
+            break;
+        }
+        case 4: {
+            auto it = st.lower_bound(k);
+            if (it == st.end()) {
+                cout << -1 << '\n';
+            } else if (*it == k) {
+                cout << k << '\n';
+            } else if (it == st.begin()) {
+                cout << -1 << '\n';
+            } else {
+                cout << *(--it) << '\n';
+            }
+        }
         }
     }
-    cout << flush;
 }
