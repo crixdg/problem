cf13b46a3c5217fe6184fede95a2753b8c80d9f9
Crix Dang <crix.dang@gmail.com>
2026-02-03 12:19:04 +0700

    up Tue Feb  3 05:19:04 AM UTC 2026

--- sources/a.cpp
+++ sources/a.cpp
@@ -1,27 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<long long> tr;
+
+void update(int n, int i, int val) {
+    for (i += n, tr[i] += val; i > 1; i >>= 1) {
+        tr[i >> 1] = tr[i] + tr[i ^ 1];
+    }
+}
+
+long long query(int n, int l, int r) {
+    long long ans = 0;
+    for (l += n, r += n; l <= r; l >>= 1, r >>= 1) {
+        if (l & 1) { ans += tr[l++]; }
+        if (!(r & 1)) { ans += tr[r--]; }
+    }
+    return ans;
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     int n, q;
     cin >> n >> q;
-
-    vector<int> lg(n + 1);
-    for (int i = 2; i <= n; i++) { lg[i] = lg[i / 2] + 1; }
-
-    vector<vector<int>> st(20, vector<int>(n));
-    for (int i = 0; i < n; i++) { cin >> st[0][i]; }
-    for (int k = 1; k < 20; k++) {
-        for (int i = 0; i + (1 << k) <= n; i++) {
-            st[k][i] = min(st[k - 1][i], st[k - 1][i + (1 << (k - 1))]);
-        }
+    tr.resize(2 * n);
+    for (int i = 0; i < n; i++) {
+        int d;
+        cin >> d;
+        update(n, i, d);
     }
     while (q--) {
-        int l, r;
-        cin >> l >> r;
-        int k = lg[r - l];
-        cout << min(st[k][l], st[k][r - (1 << k)]) << '\n';
+
+        int op;
+        cin >> op;
+        if (op == 0) {
+            int p, x;
+            cin >> p >> x;
+            update(n, p, x);
+        } else {
+            int l, r;
+            cin >> l >> r;
+            cout << query(n, l, r - 1) << '\n';
+        }
     }
 }
--- sources/data-structures/segment-tree/segment-tree.cpp
+++ sources/data-structures/segment-tree/segment-tree.cpp
@@ -1,20 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-vector<int> tree(2 * n);
+vector<long long> tr;
 
-void update(int i, int val) {
-    for (i += n, tree[i] = val; i > 1; i >>= 1) {
-        tree[i >> 1] = tree[i] + tree[i ^ 1];
+void update(int n, int i, int val) {
+    for (i += n, tr[i] += val; i > 1; i >>= 1) {
+        tr[i >> 1] = tr[i] + tr[i ^ 1];
     }
 }
 
-int query(int l, int r) {
-    int ans = 0;
+long long query(int n, int l, int r) {
+    long long ans = 0;
     for (l += n, r += n; l <= r; l >>= 1, r >>= 1) {
-        if (l & 1) { ans += tree[l++]; }
-        if (!(r & 1)) { ans += tree[r--]; }
+        if (l & 1) { ans += tr[l++]; }
+        if (!(r & 1)) { ans += tr[r--]; }
     }
     return ans;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, q;
+    cin >> n >> q;
+    tr.resize(2 * n);
+    for (int i = 0; i < n; i++) {
+        int d;
+        cin >> d;
+        update(n, i, d);
+    }
+    while (q--) {
+
+        int op;
+        cin >> op;
+        if (op == 0) {
+            int p, x;
+            cin >> p >> x;
+            update(n, p, x);
+        } else {
+            int l, r;
+            cin >> l >> r;
+            cout << query(n, l, r - 1) << '\n';
+        }
+    }
 }
