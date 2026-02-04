6c2f39b6541700d3efc3da43d4a0941b8503871d
Crix Dang <crix.dang@gmail.com>
2026-02-04 13:35:53 +0700

    up Wed Feb  4 06:35:53 AM UTC 2026

--- sources/a.cpp
+++ sources/a.cpp
@@ -2,39 +2,23 @@
 using namespace std;
 
 int n, m;
-vector<vector<int>> adj;
+vector<int> par, sz;
 
-int ti;
-vector<int> disc, low;
-vector<bool> vis;
-vector<int> stk;
-vector<vector<int>> sccs;
+int find(int x) {
+    if (par[x] == x) { return x; }
+    return par[x] = find(par[x]);
+}
 
-void dfs(int u) {
-    disc[u] = low[u] = ti++;
-    vis[u] = true;
-    stk.push_back(u);
-
-    for (int v : adj[u]) {
-        if (disc[v] == -1) {
-            dfs(v);
-            low[u] = min(low[u], low[v]);
-        } else if (vis[v]) {
-            low[u] = min(low[u], disc[v]);
-        }
+bool unite(int x, int y) {
+    int u = find(x);
+    int v = find(y);
+    if (u != v) {
+        if (sz[u] < sz[v]) { swap(u, v); }
+        par[v] = u;
+        sz[u]++;
+        return true;
     }
-
-    if (disc[u] == low[u]) {
-        vector<int> scc;
-        int v;
-        do {
-            v = stk.back();
-            stk.pop_back();
-            scc.push_back(v);
-            vis[v] = false;
-        } while (v != u);
-        sccs.push_back(scc);
-    }
+    return false;
 }
 
 int main() {
@@ -42,25 +26,30 @@ int main() {
     cin.tie(0);
 
     cin >> n >> m;
-    adj.resize(n);
+
+    par.resize(n);
+    sz.resize(n);
+    for (int i = 0; i < n; i++) { par[i] = i; }
+
+    long long sm = 0;
+    vector<int> ans;
+
+    vector<vector<int>> edges;
     for (int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({w, i, u, v});
     }
 
-    ti = 0;
-    disc.assign(n, -1);
-    low.assign(n, -1);
-    vis.resize(n);
-    for (int i = 0; i < n; i++) {
-        if (disc[i] == -1) { dfs(i); }
+    sort(begin(edges), end(edges));
+    for (auto &i : edges) {
+        if (unite(i[2], i[3])) {
+            ans.push_back(i[1]);
+            sm += i[0];
+        }
     }
 
-    cout << sccs.size() << '\n';
-    for (int i = sccs.size() - 1; i >= 0; i--) {
-        cout << sccs[i].size() << ' ';
-        for (int d : sccs[i]) { cout << d << ' '; }
-        cout << '\n';
-    }
+    cout << sm << '\n';
+    for (int d : ans) { cout << d << ' '; }
+    cout << '\n';
 }
--- sources/graph-theory/mst.cpp
+++ sources/graph-theory/mst.cpp
@@ -1,2 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int n, m;
+vector<int> par, sz;
+
+int find(int x) {
+    if (par[x] == x) { return x; }
+    return par[x] = find(par[x]);
+}
+
+bool unite(int x, int y) {
+    int u = find(x);
+    int v = find(y);
+    if (u != v) {
+        if (sz[u] < sz[v]) { swap(u, v); }
+        par[v] = u;
+        sz[u]++;
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    par.resize(n);
+    sz.resize(n);
+    for (int i = 0; i < n; i++) { par[i] = i; }
+
+    long long sm = 0;
+    vector<int> ans;
+
+    vector<vector<int>> edges;
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({w, i, u, v});
+    }
+
+    sort(begin(edges), end(edges));
+    for (auto &i : edges) {
+        if (unite(i[2], i[3])) {
+            ans.push_back(i[1]);
+            sm += i[0];
+        }
+    }
+
+    cout << sm << '\n';
+    for (int d : ans) { cout << d << ' '; }
+    cout << '\n';
+}
