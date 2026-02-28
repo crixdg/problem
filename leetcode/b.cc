0d8106d2d4ebb90cab4ccfd5eb464e2776722566
Crix Dang <crix.dang@gmail.com>
2026-02-28 14:12:55 +0700

    up Sat Feb 28 07:12:55 AM UTC 2026

---
+++ leetcode/b.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // f[i][j] i-size, j last
+    // f[i][j][vis]o
+    // f[mask][j]
+    // j in mask
+    // f[mask][j] += f[mask ^ (1<<j)][i] if nums[i] % nums[j] =
+    return 0;
+}
