4abdbb1a16e5612bf3f553cab369392b0637ec75
Crix Dang <crix.dang@gmail.com>
2026-03-03 11:50:31 +0700

    up Tue Mar  3 04:50:31 AM UTC 2026

---
+++ leetcode/a.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(vector<int>& houses, int k) {
+        int n=houses.size();
+
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums{1, 2, 3, 4, 5, 6, 7};
+    cout << sol.largestSumOfAverages(nums, 4) << endl;
+    return 0;
+}                                                                                                                                               
