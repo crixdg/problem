9be07fe767dac0b2b76963a1bd98bd0f2a29d31b
Crix Dang <crix.dang@gmail.com>
2026-03-19 10:42:27 +0700

    up Thu Mar 19 03:42:27 AM UTC 2026

---
+++ codeforces/c.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> as(n);
+    for (int &d : as) { cin >> d; }
+    war return 0;
+}
--- leetcode/a.cc
+++ leetcode/a.cc
@@ -1,41 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int longestPalindrome(string &word1, string &word2) {}
 
 int main() {
-    vector<int> nums; // input array
-                      // w(i, j);
-    auto w = [&](int i, int j) { return nums[j] - nums[i]; };
-
-    int n = nums.size();
-    vector<vector<int>> f(n, vector<int>(n));
-    vector<vector<int>> opt(n, vector<int>(n));
-
-    // base cases
-    for (int i = 0; i < n; i++) {
-        opt[i][i] = i;
-        f[i][i] = 0;
-    }
-
-    // loop backward on i because opt[i][j] depends on opt[i+1][j];
-    for (int i = n - 2; i >= 0; i--) {
-        // loop forward on j because opt[i][j] depends on opt[i][j-1];
-        for (int j = i + 1; j < n; j++) {
-            int mn = INT_MAX;
-            int cost = w(i, j);
-            // opt(i, j-1) <= opt(i, j) <= opt(i+1, j);
-            for (int o = opt[i][j - 1]; o <= min(j - 1, opt[i + 1][j]); o++) {
-                int t = f[i][o] + f[o][j] + cost;
-                if (t < mn) {
-                    opt[i][j] = o;
-                    mn = t;
-                }
-            }
-            f[i][j] = mn;
-        }
-    }
-
-    cout << f[0][n-1] << endl;
-    return 0;
+    string word1, word2;
+    cin >> word1 >> word2;
 }
-
