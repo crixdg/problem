74171f63b3fe45280688adb35d9cda14fba83514
Crix Dang <crix.dang@gmail.com>
2026-03-24 22:45:58 +0700

    up Tue Mar 24 03:45:58 PM UTC 2026

--- templates/modular.cc
+++ templates/modular.cc
@@ -3,21 +3,7 @@
 template <typename T>
 concept integer_c = std::same_as<T, int> || std::same_as<T, long> || std::same_as<T, long long>;
 
-// --------------------- MODULAR --------------------------
-
-/** modular multiplication **/
-template <integer_c T>
-T mod_multiply(T a, T b, T m) {
-  assert(m > 0);
-  T ans = 0;
-  a %= m, b %= m;
-  while (b != 0) {
-    if (b & 1) { ans = ((__int128_t)ans + a) % m; }
-    a = ((__int128_t)a + a) % m;
-    b >>= 1;
-  }
-  return ans;
-}
+// --------------------- MODULAR ARITHMETIC --------------------------
 
 /** modular inverse **/
 template <integer_c T>
@@ -36,7 +22,7 @@ T mod_inverse(T a, T m) {
 
 /** modular exponentiation **/
 template <integer_c T>
-T mod_power(T a, T b, T m) {
+T mod_power(T a, long long b, T m) {
   assert(m > 0);
   bool is_negative_power = b < 0;
   if (is_negative_power) {
@@ -90,7 +76,7 @@ public:
   modular_t &operator--() { return *this -= 1; }
   modular_t operator++(int) { modular_t result(*this); *this += 1; return result; }
   modular_t operator--(int) { modular_t result(*this); *this -= 1; return result; }
-  modular_t operator-() const { return value_ == 0 ? modular_t() : modular_t(*M_-value_); }
+  modular_t operator-() const { return value_ == 0 ? modular_t() : modular_t(-value_); }
 
   friend modular_t operator+(const modular_t &lhs, const modular_t &rhs) { return modular_t(lhs) += rhs; }
   template <integer_c U> friend modular_t operator+(const modular_t &lhs, U rhs) { return modular_t(lhs) += rhs; }
@@ -132,22 +118,58 @@ public:
   template <integer_c U> friend bool operator<=(const modular_t &lhs, U rhs) { return !(lhs > rhs); }
   template <integer_c U> friend bool operator<=(U lhs, const modular_t &rhs) { return !(lhs > rhs); }
 
+  friend modular_t power(const modular_t &lhs, long long rhs, T m) { return mod_power<T>(lhs(), rhs, m); }
+  friend modular_t inverse(const modular_t &lhs, T m) { return mod_inverse<T>(lhs(), m); }
+
 private:
   T value_;
 
   static T normalize(__int128_t val) {
     __int128_t md = static_cast<__int128_t>(*M_);
-    if (val <= -md || val >= md) { val %= md; }
-    return static_cast<T>(val < __int128_t(0) ? val + md : val);
+    val %= md; return static_cast<T>(val < __int128_t(0) ? val + md : val);
   }
 
   static T add(__int128_t a, __int128_t b) { return normalize(a + b); }
   static T sub(__int128_t a, __int128_t b) { return normalize(a - b); }
-  static T mul(__int128_t a, __int128_t b) { return mod_multiply<T>(a, b, *M_); }
+  static T mul(__int128_t a, __int128_t b) { return normalize(a * b); }
   static T div(__int128_t a, __int128_t b) { T inv = mod_inverse<T>(b, *M_); assert(inv != T(-1)); return mul(a, inv); }
+};
+
+// clang-format on
+
+// --------------------- COMBINATORICS --------------------------
+
+template <integer_c T, T *M_>
+class combinatorics_t {
+public:
+  using M = modular_t<T, M_>;
+  std::vector<M> fact, ifact;
+
+  void build(int n) {
+    fact.assign(n + 1, 1);
+    ifact.assign(n + 1, 1);
+    for (int i = 1; i <= n; i++) {
+      fact[i] = fact[i - 1] * i;
+    }
+    ifact[n] = inverse(fact[n], *M_);
+    assert(ifact[n] != M(-1));
+    for (int i = n - 1; i >= 0; i--) {
+      ifact[i] = ifact[i + 1] * (i + 1);
+    }
+  }
+
+  M C(int n, int k) {
+    if (k < 0 || k > n) { return 0; }
+    return fact[n] * ifact[k] * ifact[n - k];
+  }
+
+  M P(int n, int k) {
+    if (k < 0 || k > n) { return 0; }
+    return fact[n] * ifact[n - k];
+  }
 };
 
 using mod_t = int;
 mod_t md = 1e9 + 7;
 using mint_t = modular_t<mod_t, &md>;
-// clang-format on
+using comb_t = combinatorics_t<mod_t, &md>;
