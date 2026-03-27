a3151ec9ee0c6218375c1ae119c17ad9b1e1faa2
Crix Dang <crix.dang@gmail.com>
2026-03-27 12:54:03 +0700

    up Fri Mar 27 05:54:03 AM UTC 2026

--- unverified_templates/dsu_weighted.cc
+++ unverified_templates/dsu_weighted.cc
@@ -7,41 +7,49 @@
 
 using namespace std;
 
+template <typename Op>
+concept weighted_dsu_op_c = requires { typename Op::W; } &&
+                            requires(typename Op::W, typename Op::W a, typename Op::W b, typename Op::W d) {
+                              Op::e();
+                              Op::join(a, b);
+                              Op::diff(a, b);
+                              Op::flip(d);
+                            };
+
 /** weighted disjoint set union **/
+template <weighted_dsu_op_c Op>
 struct weighted_dsu_t {
+  using W = typename Op::W;
+
   int comp_sz;
   vector<int> p, sz;
-  vector<long long> w;
+  vector<W> w;
 
-  explicit weighted_dsu_t(int n) : comp_sz(n), p(n), w(n, 0), sz(n, 1) {
+  explicit weighted_dsu_t(int n) : comp_sz(n), p(n), w(n, Op::e()), sz(n, 1) {
     iota(p.begin(), p.end(), 0);
   }
 
-  /** find root; full path compression **/
-  std::pair<int, long long> find(int x) {
-    if (p[x] == x) { return {x, 0}; }
+  /** find root: full path compression **/
+  pair<int, W> find(int x) {
+    if (p[x] == x) { return {x, Op::e()}; }
     auto [r, dw] = find(p[x]);
-    return {p[x] = r, w[x] += dw};
+    w[x] = Op::join(w[x], dw);
+    p[x] = r;
+    return {r, w[x]};
   }
 
-  /** merge; false if not possible to link with given weight **/
-  bool unite(int x, int y, long long d) {
+  /** constraint diff(wx, wy) == d; false if contradictory **/
+  bool unite(int x, int y, W d) {
     auto [fx, wx] = find(x);
     auto [fy, wy] = find(y);
-    if (fx == fy) { return wx - wy == d; }
+    if (fx == fy) { return Op::diff(wx, wy) == d; }
     if (sz[fx] < sz[fy]) {
       swap(fx, fy);
       swap(wx, wy);
-      d = -d;
+      d = Op::flip(d);
     }
-    // p[fy] = fx => label(fx) = label(fy) + w[fy]
-    // label(x) - label(y) = d after linking
-    // label(fx) + wx - label(fy) - wy = d
-    // label(fx) + wx - (label(fx) + w[fy]) - wy = d
-    // wx - w[fy] - wy = d
-    // => w[fy] = wx - wy - d
     p[fy] = fx;
-    w[fy] = wx - wy - d;
+    w[fy] = Op::diff(Op::diff(wx, wy), d);
     sz[fx] += sz[fy];
     comp_sz--;
     return true;
@@ -50,8 +58,16 @@ struct weighted_dsu_t {
   /** check if two elements are in the same set **/
   bool same(int x, int y) { return find(x).first == find(y).first; }
 
-  /** return the weight of the set containing x **/
-  long long weight(int x) { return find(x).second; }
+  /** return the potential of x **/
+  W potential(int x) { return find(x).second; }
+
+  /** return the difference between the potential of x and y **/
+  optional<W> diff(int x, int y) {
+    auto [fx, wx] = find(x);
+    auto [fy, wy] = find(y);
+    if (fx != fy) { return nullopt; }
+    return Op::diff(wx, wy);
+  }
 
   /** return the size of the set containing x **/
   int size(int x) { return sz[find(x).first]; }
@@ -59,3 +75,41 @@ struct weighted_dsu_t {
   /** return the number of connected components **/
   int components() { return comp_sz; }
 };
+
+/** -------------------------------------------------------------- **/
+
+/** label(x) - label(y) = d (additive abelian) */
+template <class T = long long>
+struct dsu_weight_op_additive {
+  using W = T;
+  static W e() { return W(0); }
+  static W join(W a, W b) { return a + b; }
+  static W diff(W a, W b) { return a - b; }
+  static W flip(W d) { return -d; }
+};
+static_assert(weighted_dsu_op_c<dsu_weight_op_additive<long long>>);
+
+/** label(x) xor label(y) = d */
+struct dsu_weight_op_xor {
+  using W = long long;
+  static W e() { return W(0); }
+  static W join(W a, W b) { return a ^ b; }
+  static W diff(W a, W b) { return a ^ b; }
+  static W flip(W d) { return d; }
+};
+static_assert(weighted_dsu_op_c<dsu_weight_op_xor>);
+
+/** lable(x) - label(y) = d (modulo m) */
+template <class T = long long, T M_ = T(1e9 + 7)>
+struct dsu_weight_op_modulo {
+  using W = T;
+  static W e() { return W(0); }
+  static W join(W a, W b) { return (a + b) % M_; }
+  static W diff(W a, W b) { return (a - b) % M_; }
+  static W flip(W d) { return -d; }
+};
+static_assert(weighted_dsu_op_c<dsu_weight_op_modulo<long long, (long long)1e9 + 7>>);
+
+/** -------------------------------------------------------------- **/
+
+using w_dsu_t = weighted_dsu_t<dsu_weight_op_additive<long long>>;
